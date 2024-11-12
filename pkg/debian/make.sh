4ed63108891dbfa7178e888e83f6a09a2e4a334b
Trekkie <111065900+TrekkieCoder@users.noreply.github.com>
2024-11-12 09:26:22 +0900

    Update make.sh

--- pkg/debian/make.sh
+++ pkg/debian/make.sh
@@ -52,6 +52,12 @@ rm -fr openssl-3.3.1*
 
 LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:/usr/lib/"
 
+echo "openssl local build"
+ls /usr/local/build/include/openssl
+
+echo "openssl local build"
+ls /usr/include/openssl
+
 git clone --recurse-submodules https://github.com/loxilb-io/loxilb
 cd loxilb/loxilb-ebpf
 git checkout $gver
